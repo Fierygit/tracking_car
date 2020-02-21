13c2c9bf7bcfee7f71d01d6d5e567c2dc76de5e5
DMFer <2417302477@qq.com>
2020-02-21 18:52:46 +0800

    增加识别图片的程序

--- identify.py
+++ identify.py
@@ -2,25 +2,19 @@
 @Author: Firefly
 @Date: 2020-02-20 15:22:36
 @Descripttion: 
-@LastEditTime: 2020-02-21 18:05:42
+@LastEditTime: 2020-02-20 16:37:49
 '''
 
 import time
+from yolov3 import yolov3, getLocation
 
 def idnt_img(image):
-    
-    time.sleep(1.2)
-    
+    # time.sleep(1.2)
+
     # 实现识别代码
-    
+    yolov3(image)
+
     # 二元 元组
-    loc = list()
-
-    loc.append((1,2,3,4))
-    loc.append((2,3,4,5))
-
+    loc = getLocation()
+    print(loc)
     return loc
-
-def int():
-    
-
