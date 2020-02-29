13c711b70857916c3ffc721ebc8c35231be5ed1b
firefly <531109985@qq.com>
2020-02-29 23:18:45 +0800

    latest

--- tracking.py
+++ tracking.py
@@ -1,100 +1,53 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  4 14:02:25 2019
+'''
+@Author: Firefly
+@Date: 2020-02-29 22:03:44
+@Descripttion: 
+@LastEditTime: 2020-02-29 23:17:04
+'''
 
-@author: tony_xc
-"""
+from main import TrackingProcess
 
-import cv2
-import sys
-import glob
-import time
 
-(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-#print(major_ver, minor_ver, subminor_ver) # windows 3.4.5
+def tracking(fir_img, locations, fir_img_cnt, process_list):
+    visited = list()
+    cnt_loc = 0
+    for loc in locations:  # 对于每一个识别出来的车的位置
+        print("[prime tracking]: " + " width: " + str(loc[2]), end="")
+        print(" height: " + str(loc[3]) + " pro_len " + str(len(process_list)))
+        flag = 0  # 判断有没有找到
+        for li in process_list:  # 查找有没有进程在处理这辆车
+            # 一开始就固定  visited 的大小，# if visited[cnt] : break;   
+            if flag == 1: break
+            process_info = li.getInfo()
+            print("[prime tracking]: " + str(process_info))       
+            print("[prime tracking]: " + str(fir_img_cnt), end=' \t ')# 不判空一定有
+            if process_info[0][0] <= fir_img_cnt :  # 第一张必须要比之前的小
+                if len(process_info) + process_info[0][0] >= fir_img_cnt:      
+                    flag = prime_one_car(loc, process_info[fir_img_cnt])
+                    if flag == 1:
+                        break                 
+        visited.append(flag)
+        cnt_loc = cnt_loc + 1  # 处理下一辆车
+    cnt_loc = 0
 
-def getTracker(tracker_type):  #选择类型
-    if tracker_type == 'BOOSTING':
-        tracker = cv2.TrackerBoosting_create()
-    if tracker_type == 'MIL':
-        tracker = cv2.TrackerMIL_create()
-    if tracker_type == 'KCF':
-        tracker = cv2.TrackerKCF_create()
-    if tracker_type == 'TLD':
-        tracker = cv2.TrackerTLD_create()
-    if tracker_type == 'MEDIANFLOW':
-        tracker = cv2.TrackerMedianFlow_create()
-    if tracker_type == 'GOTURN':
-        tracker = cv2.TrackerGOTURN_create()
-    if tracker_type == 'MOSSE':
-        tracker = cv2.TrackerMOSSE_create()
-    if tracker_type == "CSRT":
-        tracker = cv2.TrackerCSRT_create()
+    for flag in visited:
+        if flag == 0:
+            temp_thread = TrackingProcess(fir_img)
+            temp_thread.setFirst(fir_img)  # 把 img cnt 和 车位置 加入
+            temp_thread.addInfo(([fir_img_cnt, locations[cnt_loc]], ))
+            process_list.append(temp_thread)  # 将进程加入列表， 然后 开始， 防止
+            temp_thread.start()  # 线程里面跟踪不到了就去自动停止， 内部实现
+        cnt_loc = cnt_loc + 1
 
-    return tracker
 
-if __name__ == '__main__':
+def prime_one_car(loc, p):
+    # 当前图片中点
+    mid_cur = [loc[0] + loc[2] / 2, (loc[1] + loc[3] / 2)]
+    mid_p = [(p[2] / 2 + p[0]), (p[3] / 2 + p[1])]  # p的中点
+    len_2 = (mid_p[0] - mid_cur[0]) * (mid_p[0] - mid_cur[0]) + (
+        mid_p[1] - mid_cur[1]) * (mid_p[1] - mid_cur[1])  # 距离平方
+    print(str(len_2) + '\t' + str(((loc[2]) / 2) * ((loc[2]) / 2)))
+    if (len_2 < ((loc[2]) / 2) * ((loc[2]) / 2)):
+        return 1
 
-    # Set up tracker.
-    # Instead of MIL, you can also use
-
-    tracker_types = {0:'BOOSTING', 1:'MIL', 2:'KCF', 3:'TLD',
-                     4:'MEDIANFLOW', 5:'GOTURN', 6:'MOSSE', 7:'CSRT'}
-    tracker_type = tracker_types[2]
-
-    if (int(minor_ver)<3 and int(major_ver)<3 ):
-        tracker = cv2.Tracker_create(tracker_type)
-    else:
-        tracker = getTracker(tracker_type)
-    picNum = 0
-    failureNum = 0
-    # Read jpg
-    for i in range(1,150):
-    #for jpgfile in glob.glob(r'E:\Semester_three_up\strawberry_pie\opencv-tracking\picture\*.jpg'):
-        
-        jpgfile = str("E:\Semester_three_up\\strawberry_pie\\opencv-tracking\picture\\" + str(i) + '.jpg')
-        print(jpgfile)
-
-        frame = cv2.imread(jpgfile,cv2.IMREAD_ANYCOLOR)
-        
-        picNum = picNum + 1
-        if picNum == 1 :
-            bbox = cv2.selectROI(frame, False)   
-            ok = tracker.init(frame, bbox)
-        else:
-            # Start timer
-            #timer = cv2.getTickCount()
-
-            # Update tracker
-            ok, bbox = tracker.update(frame)
-
-            # Calculate Frames per second (FPS)
-            #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-            # Draw bounding box
-            if ok:
-                # Tracking success
-                p1 = (int(bbox[0]), int(bbox[1]))
-                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-            else:
-                # Tracking failure
-                cv2.putText(frame, "Tracking failure detected", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-                failureNum = failureNum + 1
-
-            # Display tracker type on frame
-            cv2.putText(frame, tracker_type + " Tracker", (50, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
-
-            # Display FPS on frame
-            # cv2.putText(frame, "FPS : " + str(int(fps)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
-
-            # Display result
-            cv2.imshow("Tracking", frame)
-
-            # Exit if ESC pressed
-            k = cv2.waitKey(10) & 0xff
-            if k == 27: break
-            
-    time.sleep(1)
-    print(picNum)
-    print(failureNum)
-    cv2.destroyAllWindows() # close windows
+    return 0
